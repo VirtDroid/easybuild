11e4b8c7517753b9e8c866a82007fceeb1a3f269
StarNumber <64470722+StarNumber12046@users.noreply.github.com>
2022-03-09 14:37:51 +0100

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,7 +1,7 @@
 echo "Setting up normal applications"
-pacman -S firefox 
-pacman -S --needed base-devel
-pacman -S kernel26-headers file base-devel abs git
+pacman -S firefox -y
+pacman -S --needed base-devel -y
+pacman -S kernel26-headers file base-devel abs git -y
 
 echo "Installing AUR helper, using paru"
 git clone https://aur.archlinux.org/paru.git
